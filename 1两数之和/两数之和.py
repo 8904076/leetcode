c3ff1aa160461a165f71fe8a7737ff79732ee75e
陶礼之 <8904076@qq.com>
2020-04-19 23:21:44 +0800

    4.19

--- 1两数之和/两数之和.py
+++ 1两数之和/两数之和.py
@@ -1,6 +1,7 @@
 '''
 暴力穷举法
 '''
+from typing import  List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -8,3 +9,22 @@ class Solution:
                 if nums[i]+nums[j] == target:
                     return [i,j]
         return  None
+
+
+
+'''
+利用一个容器cache
+
+from typing import  List
+class Solution:
+    def twoSum(self,nums:List[int],target: int)->List[int]:
+        cache={}
+        for idx,num in enumerate(nums):
+            cur = target-num
+            if num in cache:
+                return [cache[num],idx]
+            cache[cur] = idx
+        return None
+'''
+
+
